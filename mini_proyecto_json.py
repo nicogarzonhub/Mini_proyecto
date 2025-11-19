15399aecb2ac172c1f891f6eab587ac5816f6411
nicogarzonhub <nicogarzon654@gmail.com>
2025-11-19 14:56:33 -0500

    Update mini_proyecto_json.py

--- mini_proyecto_json.py
+++ mini_proyecto_json.py
@@ -7,31 +7,27 @@ def cargar_datos():
     except:
         return []
 # Guardar datos en JSON
-
 def guardar_datos():
     with open("estudiantes.json", "w") as archivo:
         json.dump(estudiantes, archivo)
 # LISTA PRINCIPAL
-
 estudiantes = cargar_datos()
-
 # Calcular último ID
 id_actual = max([e["id"] for e in estudiantes], default=0)
-
 # Registrar estudiante
-
 def registrar_estudiante():
-    global id_actual
+    global id_actual  
     nombre = input("Escribe el nombre del estudiante: ")
     while nombre != "":
         id_actual += 1
         estudiante = {"id": id_actual, "nombre": nombre}
+
         estudiantes.append(estudiante)
         guardar_datos()
-        print(" Estudiante agregado!\n")
-        print("Para dejar de añadir estudiantes, presione enter sin ingresar nada")
-        nombre = input("Escribe el nombre del estudiante: ")
 
+        print(" Estudiante agregado!\nPara salir presione enter sin ingresar nada")
+        nombre = input("ingrese otro nombre de estudiante: ")
+#mostrar lista de estidiantes
 def mostrar_estudiante():
     if len(estudiantes) == 0:
         print("No hay estudiantes todavía\n")
@@ -40,15 +36,11 @@ def mostrar_estudiante():
         for e in estudiantes:
             print(f'ID: {e["id"]} - Nombre: {e["nombre"]}')
         print()
-
-
 # Eliminar estudiante por ID
-
 def eliminar_estudiante():
     if len(estudiantes) == 0:
         print("No hay estudiantes para eliminar\n")
         return
-
     try:
         id_buscar = int(input("Ingresa el ID del estudiante a eliminar: "))
     except:
@@ -60,24 +52,17 @@ def eliminar_estudiante():
             guardar_datos()
             print(" Estudiante eliminado!\n")
             return
-
     print("No existe un estudiante con ese ID.\n")
-
-
-
 # Actualizar estudiante por ID
-
 def actualizar_estudiante():
     if len(estudiantes) == 0:
         print("No hay estudiantes para actualizar\n")
         return
-
     try:
         id_buscar = int(input("Ingresa el ID del estudiante a actualizar: "))
     except:
         print("ID inválido\n")
         return
-
     for e in estudiantes:
         if e["id"] == id_buscar:
             nuevo = input("Nuevo nombre: ")
@@ -85,13 +70,8 @@ def actualizar_estudiante():
             guardar_datos()
             print(" Estudiante actualizado!\n")
             return
-
     print(" No existe un estudiante con ese ID.\n")
-
-
-
 # MENÚ
-
 while True:
     print("= MENÚ PRINCIPAL =")
     print("1. Registrar estudiante")
@@ -99,10 +79,7 @@ while True:
     print("3. Eliminar estudiante")
     print("4. Actualizar estudiante")
     print("5. Salir")
-  
-
     opcion = input("Elige una opción (1-4): ")
-
     if opcion == "1":
         registrar_estudiante()
     elif opcion == "2":
